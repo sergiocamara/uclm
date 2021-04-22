985fd0bad586bb4f9314fc0e4f0a6fe7fd0508e3
Raul Cámara Sevilla <rcamara@sicaman-nt.com>
2021-04-22 22:04:35 +0200

    Fix

--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 server = Flask(__name__)
 
 @server.route("/")
- def hello():
+def hello():
     return "Hola planificación y gestión de infraestructuras TIC \n\n Por Sergio Cámara"
 
 if __name__ == "__main__":
