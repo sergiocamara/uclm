8c3c318e44f56e625a5265d2da79877505b9e622
sergiocamara <62543120+sergiocamara@users.noreply.github.com>
2021-04-22 21:46:08 +0200

    Server py

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+server = Flask(__name__)
+
+@server.route("/")
+ def hello():
+    return "Hola planificación y gestión de infraestructuras TIC \n\n Por Sergio Cámara"
+
+if __name__ == "__main__":
+   server.run(host='0.0.0.0')
