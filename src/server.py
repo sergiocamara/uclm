23ec83f4f83e0b6fd23ac65a70158fbd2bfcb180
sergiocamara <62543120+sergiocamara@users.noreply.github.com>
2021-04-25 13:35:51 +0200

    strong

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "<h1>Hola Planificación y Gestión de infraestructuras TIC</h1> </br></br> <h2>Por Sergio Cámara</h2>"
+    return "<h1>Hola Planificación y Gestión de infraestructuras TIC</h1> </br></br> <h2>Por <strong>Sergio Cámara</strong></h2>"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
